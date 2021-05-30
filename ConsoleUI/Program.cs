0e2fe0898dbf2fc8a560e1fa67f7e2e2aab05590
yahyasezgin <yahya_sezginys@hotmail.com>
2021-05-30 23:26:23 +0300

    9. ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,10 +9,26 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var item in productManager.GetByUnitPrice(40,100))
+            ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var item in categoryManager.GetAll())
             {
-                Console.WriteLine(item.ProductName);
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetProductDetails())
+            {
+                Console.WriteLine("ProductName: " + item.ProductName + " CategoryName: " + item.CategoryName);
             }
         }
     }
